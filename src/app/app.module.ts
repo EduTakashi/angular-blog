f61b800bfaea0a3ffed107f662ca7af760156f88
Eduardo Takashi Missaka <10417877@mackenzista.com.br>
2025-03-17 22:51:14 -0300

    Atualizacao do menu-title

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,7 +6,7 @@ import { AppComponent } from './app.component';
 import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
 import { MenuTitleComponent } from './components/menu-title/menu-title.component';
 import { BigCardComponent } from './components/big-card/big-card.component';
-import { SmallCardComponent } from './small-card/small-card.component';
+import { SmallCardComponent } from './components/small-card/small-card.component';
 
 @NgModule({
   declarations: [
